ba35bb1cbb67a5e60f2bf03e93f31927fd2e2a19
Habray4 <egor.svib@gmail.com>
2025-10-05 21:08:22 +0300

    Add files via upload

---
+++ lab 2/B1/B1 - map distance.py
@@ -0,0 +1,28 @@
+import sys
+import math
+data_path = "./" + str(sys.argv[1])
+data = open(data_path, "r")
+print("Egor Svibovich")
+print("Simple Map Distance Computations")
+print("")
+dist_count, map_scale = map(float, data.readline().split())
+dist_count = int(dist_count) 
+map_scale = math.ceil((map_scale) * 100) / 100
+print(f"Map Scale Factor: {map_scale:>7} miles per inch")
+distances = data.read().split("\n")
+distances = distances[:-1]
+distances = list(map(float, distances))
+for dist in distances:
+    dist = math.ceil((dist) * 10) / 10
+print("      Map       Mileage")
+print("      Measure   Distance")
+print("============================================================")
+total_dist = float(0)
+for i in range(1, dist_count + 1):
+    real_dist = distances[i - 1]
+    real_dist = math.ceil((real_dist * map_scale) * 10) / 10
+    total_dist += real_dist
+    print(f"# {i:<4} {distances[i - 1]:<9} {real_dist}")
+print("============================================================")
+print(f"Total Distance: {total_dist:>6} miles")
+data.close()
